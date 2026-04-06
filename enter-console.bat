d714022d09514d05eca9ed296cb8c09425036ca5
Intisy <finn@birich.de>
2026-04-06 23:20:42 +0200

    Split data dirs into submodules, add Ark ASE and ASA servers

--- enter-console.bat
+++ enter-console.bat
@@ -4,11 +4,15 @@ echo Select which console to enter:
 echo 1) Minecraft Server
 echo 2) Hytale Server
 echo 3) Playit Agent
-set /p choice=Enter choice [1-3]: 
+echo 4) Ark: Survival Evolved Server
+echo 5) Ark: Survival Ascended Server
+set /p choice=Enter choice [1-5]: 
 
 if "%choice%"=="1" goto minecraft
 if "%choice%"=="2" goto hytale
 if "%choice%"=="3" goto playit
+if "%choice%"=="4" goto ark_ase
+if "%choice%"=="5" goto ark_asa
 echo Invalid choice.
 pause
 exit
@@ -26,6 +30,16 @@ goto end
 :playit
 echo Attaching to Playit Agent... (Press Ctrl+P, Ctrl+Q to detach)
 docker attach playit_agent
+goto end
+
+:ark_ase
+echo Attaching to Ark: Survival Evolved Server... (Press Ctrl+P, Ctrl+Q to detach)
+docker attach ark_ase_server
+goto end
+
+:ark_asa
+echo Attaching to Ark: Survival Ascended Server... (Press Ctrl+P, Ctrl+Q to detach)
+docker attach ark_asa_server
 goto end
 
 :end
--- enter-console.sh
+++ enter-console.sh
@@ -4,7 +4,9 @@ echo "Select which console to enter:"
 echo "1) Minecraft Server"
 echo "2) Hytale Server"
 echo "3) Playit Agent"
-read -p "Enter choice [1-3]: " choice
+echo "4) Ark: Survival Evolved Server"
+echo "5) Ark: Survival Ascended Server"
+read -p "Enter choice [1-5]: " choice
 
 case $choice in
     1)
@@ -18,6 +20,14 @@ case $choice in
     3)
         echo "Attaching to Playit Agent... (Press Ctrl+P, Ctrl+Q to detach)"
         docker attach playit_agent
+        ;;
+    4)
+        echo "Attaching to Ark: Survival Evolved Server... (Press Ctrl+P, Ctrl+Q to detach)"
+        docker attach ark_ase_server
+        ;;
+    5)
+        echo "Attaching to Ark: Survival Ascended Server... (Press Ctrl+P, Ctrl+Q to detach)"
+        docker attach ark_asa_server
         ;;
     *)
         echo "Invalid choice."
